7c6f96e43b0fc1ac252c373e8ead5af843e41d3b
ParkEunwoo <dmsdn960@gmail.com>
2022-02-26 23:17:20 +0900

    파이어베이스 스토리지 함수

---
+++ src/firebase/storage.js
@@ -0,0 +1,10 @@
+import { getStorage, ref, uploadBytes } from 'firebase/storage';
+import app from './firebase';
+
+const storage = getStorage(app);
+
+export const uploadFile = (eventId, file) => {
+  // TODO: 파일 경로, 파일 확장자 지정
+  const imageRef = ref(storage, `${eventId}/${file.name}.${file.ext}`);
+  return uploadBytes(imageRef, file);
+}
