93b6aef0cc56a46643e795377d99d1319384aea7
ParkEunwoo <dmsdn960@gmail.com>
2022-03-01 16:48:22 +0900

    login test page

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import { createUser, getCurrentUser, login } from './firebase/auth';
-import { getUser } from './firebase/firestore';
+import Auth from './Auth';
 
 function App() {
-  const [email, setEmail] = useState('dmsdn960@gmail.com');
-  const [password, setPassword] = useState('123456');
-  const [name, setName] = useState('박은우');
-  const [phoneNumber, setPhoneNumber] = useState('01012345678');
-
   return (
     <div className="App">
       <header className="App-header">
-        <label>
-          Email:
-          <input value={email} onChange={e => setEmail(e.target.value)} />
-        </label>
-        <label>
-          Password:
-          <input value={password} onChange={e => setPassword(e.target.value)} />
-        </label>
-        <label>
-          Name:
-          <input value={name} onChange={e => setName(e.target.value)} />
-        </label>
-        <label>
-          phone:
-          <input value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} />
-        </label>
-
-        <button onClick={() => {createUser({email, password, name, phoneNumber})}}>Sign Up</button>
-        <button onClick={() => {
-          login('dmsdn960@gmail.com', '123456')
-            .then(() => getCurrentUser())
-            .then((user) => {
-              console.log(user.displayName);
-              return getUser(user.displayName)
-            })
-            .then(user => {
-              console.log(user)
-            })
-        }}>Login</button>
+        <Auth />
       </header>
     </div>
   );
