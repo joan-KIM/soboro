2389fc1685066d0e58bfa1e0b677ed645a74a5b3
joan-KIM <3373minji.kim@gmail.com>
2022-04-08 16:29:04 +0900

    회원가입 페이지 마크업 #15

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import Auth from '../components/Auth';
 import SignUpInput from '../components/SignUpInput';
 import styled from 'styled-components';
 import {useForm} from 'react-hook-form';
+import {ReactComponent as Close} from '../assets/close.svg';
 
 const Page = styled.div`
   padding: 17px;
@@ -18,8 +18,8 @@ export default function SignUpPage() {
 
   return (
     <Page>
-      <h1>회원가입 페이지</h1>
-      <Link to="/account/login">뒤로가기</Link>
+      <Link to="/account/login"><Close /></Link>
+      <p>회원가입</p>
       <form onSubmit={handleSubmit(onSubmit)}>
         <SignUpInput
           required
@@ -35,6 +35,19 @@ export default function SignUpPage() {
           error={errors.username}
         />
         <SignUpInput
+          required
+          name="email"
+          label="이메일"
+          register={register}
+          placeholder="abcde@gmail.com"
+          reset={reset}
+          validate={{
+            minLength: (v) => v.length > 6 || '최소 6자 이상 입력해주세요.',
+          }}
+          isDirty={dirtyFields.email}
+          error={errors.email}
+        />
+        <SignUpInput
           password
           required
           name="password"
@@ -48,9 +61,55 @@ export default function SignUpPage() {
           isDirty={dirtyFields.password}
           error={errors.password}
         />
+        <SignUpInput
+          password
+          required
+          name="confirmPassword"
+          label="비밀번호 확인"
+          register={register}
+          placeholder="비밀번호 재입력"
+          reset={reset}
+          validate={{
+            minLength: (v) => v.length > 6 || '최소 6자 이상 입력해주세요.',
+          }}
+          isDirty={dirtyFields.confirmPassword}
+          error={errors.confirmPassword}
+        />
+        <SignUpInput
+          required
+          name="birthday"
+          label="생년월일"
+          register={register}
+          placeholder="숫자 6자리 입력"
+          reset={reset}
+          validate={{
+            minLength: (v) => v.length > 6 || '최소 6자 이상 입력해주세요.',
+          }}
+          isDirty={dirtyFields.birthday}
+          error={errors.birthday}
+        />
+        <ul>
+          <li>
+            <label>
+              <input type="checkbox" />
+              (필수) 개인정보 수집 및 이용 동의
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type="checkbox" />
+              (필수) 추억을 보낸 친구와 타임라인 공유하기
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type="checkbox" />
+              (필수) 성실하게 추억을 기록하고 나누기
+            </label>
+          </li>
+        </ul>
         <input type="submit" value="확인" />
       </form>
-      <Auth />
     </Page>
   );
 }
