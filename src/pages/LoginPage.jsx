465501e449712fa11b94328e0991b24acd503fd5
joan-KIM <3373minji.kim@gmail.com>
2022-04-13 21:18:29 +0900

    회원가입 구현 #17

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Auth from '../components/Auth';
+import {Link} from 'react-router-dom';
 
 export default function LoginPage() {
   return (
     <div>
       <h1>로그인 페이지</h1>
+      <Link to="/account/signup">회원가입 페이지로 이동</Link>
       <Auth />
     </div>
   );
