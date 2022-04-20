1a272bd720fab5db8c2e288101e6aa8738e77778
joan-KIM <3373minji.kim@gmail.com>
2022-04-20 13:07:33 +0900

    로그인 입력창 디자인 적용 #36

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Auth from '../components/Auth';
 import {Link} from 'react-router-dom';
+import LoginInput from '../components/LoginInput';
+import styled from 'styled-components';
+
+const Page = styled.div`
+  padding: 21px;
+`;
 
 export default function LoginPage() {
   return (
-    <div>
+    <Page>
       <h1>로그인 페이지</h1>
       <Link to="/account/signup">회원가입 페이지로 이동</Link>
+      <LoginInput placeholder="이메일 주소" />
       <Auth />
-    </div>
+    </Page>
   );
 }
