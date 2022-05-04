a568f8adbc5888efc96e67c696b8b27e5d204b82
ParkEunwoo <dmsdn960@gmail.com>
2022-05-04 22:36:40 +0900

    storage 관련 hook

--- src/firebase/storage.js
+++ src/firebase/storage.js
@@ -1,4 +1,4 @@
-import {getStorage, ref, uploadBytesResumable} from 'firebase/storage';
+import {getStorage, ref, uploadBytesResumable, getDownloadURL} from 'firebase/storage';
 import app from './firebase';
 
 const storage = getStorage(app);
@@ -6,4 +6,9 @@ const storage = getStorage(app);
 export const uploadFile = (id, file) => {
   const imageRef = ref(storage, `${id}/${file.name}`);
   return uploadBytesResumable(imageRef, file);
+};
+
+export const getURL = (id, fileName) => {
+  const imageRef = ref(storage, `${id}/${fileName}`);
+  return getDownloadURL(imageRef);
 };
--- src/pages/ProfilePhotoPage.jsx
+++ src/pages/ProfilePhotoPage.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useForm} from 'react-hook-form';
 import ProfileUploader from '../components/ProfileUploader';
 import {useAuth} from '../hooks/useAuth';
-import {uploadFile} from '../firebase/storage';
 import Button from '../components/common/Button';
 import {compress} from '../utils/compress';
+import {useStorage} from '../hooks/useStorage';
+import {getURL} from '../firebase/storage';
 
 export default function ProfilePhotoPage() {
   const {register, handleSubmit} = useForm();
   const {user} = useAuth();
+  const {upload} = useStorage(user?.uid);
+  const [url, setPhotoUrl] = useState();
 
   const onSubmit = async ({profile}) => {
     const [file] = profile;
@@ -16,21 +19,24 @@ export default function ProfilePhotoPage() {
     // 1. storage에 이미지를 저장
     // 2. 유저 db(firestore)에 이미지 주소를 저장
 
-    // let photoUrl = '';
+    let photoUrl = '';
     // file이 있는 경우
     if (file) {
       const compressedFile = await compress(file, 120, 120);
-      uploadFile(user.uid, compressedFile);
+      photoUrl = await upload(compressedFile);
     } else {
       // 기본이미지중 랜덤으로 하나 선택
+      const n = Math.ceil(Math.random() * 7);
+      photoUrl = await getURL('default_image', n+'.png');
     }
-
+    setPhotoUrl(photoUrl);
     // 유저 db에 프로필 url 업데이트
   };
 
   return (
     <div>
       <h1>프로필 사진 추가 페이지</h1>
+      <img src={url} alt="프로필 사진" />
       <form onSubmit={handleSubmit(onSubmit)}>
         <ProfileUploader name="profile" register={register} />
         <Button type="submit" value="시작하기" />
