aa938624ac93b02d3e140e28ba2ca8722360c74e
ParkEunwoo <dmsdn960@gmail.com>
2022-05-08 19:50:55 +0900

    timeline 로직 구현

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -12,7 +12,6 @@ import {
   query,
   where,
   getDocs,
-  orderBy,
   runTransaction,
 } from 'firebase/firestore';
 import app from './firebase';
@@ -41,7 +40,9 @@ export const getEvent = (id) => {
 
 export const getEvents = async () => {
   const eventsSnapshot = await getDocs(eventsRef);
-  return eventsSnapshot.map((doc) => doc.data());
+  const events = [];
+  eventsSnapshot.forEach((doc) => events.push(doc.data()));
+  return events;
 };
 
 export const getEventRef = (id) => {
@@ -200,49 +201,4 @@ export const removeFriend = (user, uid) => {
       'friend.list': arrayRemove(user.uid),
     });
   });
-};
-
-// TODO: 복잡한 필터 없이 전체 타임라인 가져오기로 변경
-export const getTimeline = async (user, options) => {
-  // 1. current user로 부터 멤버를 가져온다.
-  const followers = [
-    options.isIncludingMe && user.uid,
-    ...(options.members ?
-      options.members :
-      getFriends(user).map(({uid}) => uid)),
-  ];
-  // 2. 멤버가 모두 포함된 이벤트를 가져온다.
-  // 해당 문서에 속한 멤버들이 전부 followers에 속하는지 확인.
-
-  // 멤버, 팔로워들
-  // 이벤트의 멤버가 모두 내 팔로워들에 속한 이벤트들
-  // 이벤트의 멤버 한명한명 다 내 팔로워들에 속해있는지 확인해야함. 그리고 전부 속해있을때 조건 성립.
-
-  // 하지만 지금 주어진 것
-  // array-contains
-  // 문서의 배열에 특정 값이 존재하는지 확인하는 연산자.
-  // array-contains-any
-  // 문서의 배열에 여러개의 값 or로 존재하는지 확인
-  // 이벤트의 멤버에 특정 값이 속해있는지 확인
-
-  // 내  팔로어 = (A, B, C)
-
-  // 이벤트 멤버 = (A, B, C, D)
-  const q = options ?
-    query(eventsRef,
-        where('title', '>=', options.keyword),
-        where('description', '>=', options.keyword),
-        where('date', '>=', options.startDate),
-        where('date', '<=', options.endDate),
-        where('members', 'array-contains-any', followers),
-        orderBy('date', 'desc'),
-    ) :
-    query(eventsRef,
-        where('members', 'array-contains-any', followers),
-        orderBy('date', 'desc'),
-    );
-
-  const querySnapshot = await getDocs(q);
-  return querySnapshot.map((doc) => doc.data())
-      .filter(({members}) => members.every((member) => followers.includes(member)));
 };
