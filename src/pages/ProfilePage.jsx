752d8649de250de1268f8f65ca734ceddf8e95da
joan-KIM <3373minji.kim@gmail.com>
2022-05-11 02:37:08 +0900

    상단바 마크업, 디자인 적용

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -5,23 +5,49 @@ import styled from 'styled-components';
 
 const Page = styled.div`
   background: #E5E5E5;
+  height: 100vh;
+`;
+
+const Header = styled.div`
+  background: #FFFFFF;
+  width: 100vw;
+  height: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  a, a:visited{
+    text-decoration: none;
+    color: black;
+    font-weight: 600;
+    display: block;
+    position: absolute;
+    right: 22px;
+  }
+`;
+
+const Title = styled.p`
+  font-weight: 600;
+  font-size: 18px;
+  margin: 0;
 `;
 
 export default function ProfilePage() {
-  const {logout} = useAuth();
+  const {user} = useAuth();
   return (
     <Page>
-      <h1>마이페이지</h1>
-      <Link to="/">뒤로가기</Link>
-      <ul>
-        <li>
-          <Link to="/profile/edit">마이페이지 수정</Link>
-        </li>
-        <li>
-          <Link to="/friends/list">팔로우 목록</Link>
-        </li>
-        <button onClick={() => logout()}>로그아웃</button>
-      </ul>
+      <Header>
+        <Title>프로필</Title>
+        <Link to="/profile/edit">수정</Link>
+      </Header>
+      <div>
+        프로필 컴포넌트
+        <p>{user?.name}</p>
+        <p>{user?.email}</p>
+        <div>
+          친구/추억
+        </div>
+      </div>
     </Page>
   );
 }
