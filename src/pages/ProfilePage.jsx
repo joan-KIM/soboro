66e52b61345928cdd25263dca3df92c281648fa6
joan-KIM <3373minji.kim@gmail.com>
2022-05-11 04:26:23 +0900

    상단바 고정 / 페이지 상단 마크업 및 디자인 적용, 정렬 #65

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom';
 import {useAuth} from '../hooks/useAuth';
 import styled from 'styled-components';
 import ProfilePicture from '../components/ProfilePicture';
+import {useFriends} from '../hooks/useFriends';
+import {useTimeline} from '../hooks/useTimeline';
 
 const Page = styled.div`
   background: #E5E5E5;
@@ -16,6 +18,8 @@ const Header = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  position: sticky;
+  top: 0;
 
   a, a:visited{
     text-decoration: none;
@@ -27,28 +31,92 @@ const Header = styled.div`
   }
 `;
 
+const Main = styled.main`
+  height: 100%;
+`;
+
 const Title = styled.p`
   font-weight: 600;
   font-size: 18px;
   margin: 0;
 `;
 
+const Id = styled.p`
+  font-family: 'Suez One', serif;
+  font-size: 30px;
+  margin: 4px 0 3px;
+`;
+
+const Email = styled.p`
+  font-size: 15px;
+  font-weight: 500;
+  color: #5E5E5E;
+  margin: 3px 0;
+`;
+
+const Span = styled.span`
+  font-size: 13px;
+  color: #5E5E5E;
+`;
+
+const P = styled.p`
+  font-size: 32px;
+  font-weight: 600;
+  margin: 0;
+  line-height: 90%;
+`;
+
+const Line = styled.div`
+  height: 27px;
+  border-left: 0.5px solid #BDBDBD;
+  margin: 20px;
+`;
+
+const Wrapper = styled.div`
+  // background: yellow;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const View = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 28px 0;
+  // background: red;
+`;
+
 export default function ProfilePage() {
   const {user} = useAuth();
+  const friends = useFriends();
+  const {timelineWithMe} = useTimeline();
+
   return (
     <Page>
       <Header>
         <Title>프로필</Title>
         <Link to="/profile/edit">수정</Link>
       </Header>
-      <div>
-        <ProfilePicture />
-        <p>{user?.name}</p>
-        <p>{user?.email}</p>
-        <div>
-          친구/추억
-        </div>
-      </div>
+
+      <Main>
+        <View>
+          <ProfilePicture />
+          <Id>{user?.name}</Id>
+          <Email>{user?.email}</Email>
+          <Wrapper>
+            <div>
+              <P>{friends.length}</P>
+              <Span>친구</Span>
+            </div>
+            <Line />
+            <div>
+              <P>{timelineWithMe.length}</P>
+              <Span>추억</Span>
+            </div>
+          </Wrapper>
+        </View>
+      </Main>
     </Page>
   );
 }
